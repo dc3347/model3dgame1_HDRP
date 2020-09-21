7bcda02f13db131ccc8b73fa3ae4ea255a21151a
Danielle Cai <dc3347@columbia.edu>
2020-09-21 14:23:26 -0400

    initializing project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 12f;
+    public CharacterController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/SliderControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderControl : MonoBehaviour
+{   
+
+    public GameObject plane;
+
+    public void Slider_Changed(float newValue){
+        Vector3 pos = plane.transform.position;
+        pos.y = newValue;
+        plane.transform.position = pos;
+    }
+
+}
